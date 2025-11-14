32bce54984b947d0e23193f27bdf0f4bde774e10
Arockiya Ashika <arockiyaashika7701@gmail.com>
2025-11-14 06:53:52 -0800

    allowed hosts update

--- onlinecourse/settings.py
+++ onlinecourse/settings.py
@@ -17,7 +17,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # -------------------------------
 SECRET_KEY = config('SECRET_KEY', default='replace_this_with_env_secret')
 DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*').split(',')
+ALLOWED_HOSTS = ["*", "onlinecourse-production.up.railway.app"]
+
 
 # -------------------------------
 # APPLICATION DEFINITION
